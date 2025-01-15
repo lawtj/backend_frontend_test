767d5a993d6a5dcc965138b163434b9f02336e49
lawtj <41483192+lawtj@users.noreply.github.com>
2025-01-15 07:44:48 -0800

    change to recommended svelte secrets

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,9 +1,9 @@
-const backendUrl = import.meta.env.VITE_API_SERVER_URL;
+import { API_SERVER_URL } from '$env/static/private';
 
 export const actions = {
     default: async ({ request }) => {
         // now hit the backend health api
-        const response = await fetch(`${backendUrl}/health`);
+        const response = await fetch(`${API_SERVER_URL}/health`);
         const data = await response.json();
         console.log(data);
         return { success: true, message: "Health check successful" };
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">  
-
+    import { PUBLIC_API_CLIENT_URL, PUBLIC_PB_CLIENT_URL } from '$env/static/public';
     let {data, form} = $props();
    
-    const apiClientUrl = import.meta.env.VITE_API_CLIENT_URL;
-    const apiServerUrl = import.meta.env.VITE_API_SERVER_URL;
-    const pbServerUrl = import.meta.env.VITE_PB_SERVER_URL;
-    const pbClientUrl = import.meta.env.VITE_PB_CLIENT_URL;
-    
     const healthCheck = async () => {
-        const response = await fetch(`${apiClientUrl}/health`);
+        const response = await fetch(`${PUBLIC_API_CLIENT_URL}/health`);
         const data = await response.json();
         console.log(data);
         healthresponse = JSON.stringify(data);
@@ -38,10 +33,9 @@
             <button class="btn btn-primary" type="submit">Health Check (backend)</button>
         </form>
     </div>
-    <p>The frontend API URL is: {apiClientUrl}</p>
-    <p>The backend API URL is: {apiServerUrl}</p>
-    <p>The PocketBase server URL is: {pbServerUrl}</p>
-    <p>The PocketBase client URL is: {pbClientUrl}</p>
+    <p>The frontend API URL is: {PUBLIC_API_CLIENT_URL}</p>
+
+    <p>The PocketBase client URL is: {PUBLIC_PB_CLIENT_URL}</p>
     <p>{healthresponse}</p>
     {#if form?.success}
         <p>{form.message}</p>
